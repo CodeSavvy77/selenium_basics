91b8bb8b2d51bcf8aaeee11603d1cafd07aaca5e
Sachin <sachinkujurcs13@gmail.com>
2023-09-26 17:49:26 +0530

    JUnit 4 Assertions

--- src/main/java/SeleniumRunner.java
+++ src/main/java/SeleniumRunner.java
@@ -1,9 +1,14 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+import java.util.stream.Collectors;
 
 public class SeleniumRunner {
 
@@ -11,14 +16,23 @@ public class SeleniumRunner {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver();
         driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get("https://github.com/");
 
         WebElement searchInput = driver.findElement(By.cssSelector("body > div.logged-out.env-production.page-responsive.header-overlay.home-campaign > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu--logged-out.p-responsive.height-fit.position-lg-relative.d-lg-flex.flex-column.flex-auto.pt-7.pb-4.top-0 > div > div > qbsearch-input > div.search-input-container.search-with-dialog.position-relative.d-flex.flex-row.flex-items-center.mr-4.rounded > button > span"));
+
+        String searchPhrase = "selenium";
         searchInput.click();
         WebElement searchInput1 = driver.findElement(By.cssSelector("#query-builder-test"));
-        searchInput1.sendKeys("Selenium");
+        searchInput1.sendKeys(searchPhrase);
         searchInput1.sendKeys(Keys.ENTER);
 
+        List<String> actualItems = driver.findElements(By.cssSelector(".Box-sc-g0xbh4-0.bBwPjs.search-title")).stream().map(webElement -> webElement.getText().toLowerCase()).collect(Collectors.toList());
+//        Assert.assertTrue(actualItems.stream().allMatch(item -> item.contains(searchPhrase)));
+
+        List<String> expectedItems = actualItems.stream().filter(item -> item.contains(searchPhrase)).collect(Collectors.toList());
+        Assert.assertEquals(expectedItems, actualItems);
+        
         driver.quit();
     }
 }
