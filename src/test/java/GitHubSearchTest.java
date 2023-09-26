e8ac98920c5a4432592ad335113b9bcdf7fbe0c9
Sachin <sachinkujurcs13@gmail.com>
2023-09-26 18:25:02 +0530

    First test in JUnit 5

--- src/test/java/GitHubSearchTest.java
+++ src/test/java/GitHubSearchTest.java
@@ -1,5 +1,8 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
@@ -10,13 +13,20 @@ import java.time.Duration;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class SeleniumRunner {
+public class GitHubSearchTest {
 
-    public static void main(String[] args) {
+    private static WebDriver driver;
+
+    @BeforeAll
+    public static void setUpDriver(){
         WebDriverManager.chromedriver().setup();
-        WebDriver driver = new ChromeDriver();
+        driver = new ChromeDriver();
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+    }
+
+    @Test
+    public void checkGitHubSearch(){
         driver.get("https://github.com/");
 
         WebElement searchInput = driver.findElement(By.cssSelector("body > div.logged-out.env-production.page-responsive.header-overlay.home-campaign > div.position-relative.js-header-wrapper > header > div > div.HeaderMenu--logged-out.p-responsive.height-fit.position-lg-relative.d-lg-flex.flex-column.flex-auto.pt-7.pb-4.top-0 > div > div > qbsearch-input > div.search-input-container.search-with-dialog.position-relative.d-flex.flex-row.flex-items-center.mr-4.rounded > button > span"));
@@ -31,7 +41,10 @@ public class SeleniumRunner {
 
         List<String> expectedItems = actualItems.stream().filter(item -> item.contains(searchPhrase)).collect(Collectors.toList());
         Assertions.assertEquals(expectedItems, actualItems);
-        
+    }
+
+    @AfterAll
+    public static void tearDownDriver(){
         driver.quit();
     }
 }
